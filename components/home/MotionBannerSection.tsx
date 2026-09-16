cb2f343979b978c485a1485e0305cc2d8e2661de
aniketthakur-404 <anikett331@gmail.com>
2026-09-16 12:36:17 +0530

    banner update

--- components/home/MotionBannerSection.tsx
+++ components/home/MotionBannerSection.tsx
@@ -2,15 +2,15 @@ import Image from "next/image"
 
 export function MotionBannerSection() {
   return (
-    <section className="relative h-[100svh] w-full overflow-hidden bg-black">
+    <section className="relative aspect-[2172/724] w-full overflow-hidden bg-black">
       <Image
-        src="/images/products/product8.png"
-        alt="Editorial fashion image"
+        src="/home page content/ChatGPT Image Sep 16, 2026, 12_32_15 PM.png"
+        alt="Editorial fashion banner"
         fill
         sizes="100vw"
         className="object-cover object-center"
-        priority
       />
     </section>
   )
 }
+
